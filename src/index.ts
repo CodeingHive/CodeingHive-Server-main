bfc13d1973d57ce230330fcbd078dec8a1142c28
Himasnhu-AT <aristech.official@gmail.com>
2023-10-16 02:01:31 +0530

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 import codeRouter from './route/submit-code';
 
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Mount the submit-code route at '/submit-code'
 app.use('/submit-code', codeRouter);
